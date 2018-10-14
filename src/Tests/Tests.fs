571a4ed2ca3a057462def33667323495efbeb75e
tcooper8 <coopetyl@live.com>
2018-10-14 15:09:58 -0600

    Added tests for Graphql

--- src/Tests/Tests.fs
+++ src/Tests/Tests.fs
@@ -6,6 +6,16 @@ open Xunit
 open Services
 open Platform
 open Common.Eventing
+open Common.Users
+
+type 'a GraphqlResp = {
+  data: 'a
+  errors: obj list
+}
+
+type NewUserResp = {
+  newUser: string
+}
 
 let services:Platform.Services =
   { users = UserService.init()
@@ -23,7 +33,7 @@ let ``Test new user`` () = async {
       | RelationCreated (_, UserEmail) -> ()
       | any -> failwithf "Event %A is unexpected" any
     )
-  
+
   do! platform.Init()
   let! userId =
     platform.NewUser
@@ -34,4 +44,28 @@ let ``Test new user`` () = async {
         MailAddress "bob3@gmail.com"
       ]
   printfn "Added user %A" userId
+}
+
+[<Fact>]
+let ``Test newUse mutation`` () = async {
+  let userInfo =
+    { id = UserId <| System.Guid.NewGuid()
+      name = "bob"
+      email = MailAddress "bob@gmail.com"
+    }
+  let query =
+    """ mutation NewUser {
+          newUser(name: "bob") {
+            name
+          }
+        }
+    """
+
+  let! (data, errors) = Graphql.Resolvers.direct query userInfo
+  let json = Encoding.Json.toString data
+  let (Choice1Of2 res) = Encoding.Json.tryParse<NewUserResp GraphqlResp> json
+
+  let (UserId id) = userInfo.id
+  if res.data.newUser <> string id then
+    failwithf "Expected id `%s` but got `%s`" (string id) res.data.newUser
 }
