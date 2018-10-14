ab9ea04788a6d48048855d885fc31c0c5eda60c0
tcooper8 <coopetyl@live.com>
2018-10-14 12:46:51 -0600

    Updated Graphql

--- src/Cli/Program.fs
+++ src/Cli/Program.fs
@@ -1,6 +1,17 @@
 ﻿open System
 
+open System.Net.Mail
+open Common.Users
+
 [<EntryPoint>]
 let main argv =
-  printfn "BAM!"
+  let userInfo =
+    { id = UserId <| System.Guid.NewGuid()
+      name = "bob"
+      email = MailAddress "bob@gmail.com"
+    }
+  printf "Query >>> "
+  let line = Console.ReadLine()
+  let reply = Graphql.Resolvers.exec.AsyncExecute(queryOrMutation = line, data = userInfo) |> Async.RunSynchronously
+  printfn "%A" reply
   0
--- src/Graphql/Library.fs
+++ src/Graphql/Library.fs
@@ -1,16 +1,57 @@
 namespace Graphql
 
 open System
+open System.Net.Mail
 open FSharp.Data.GraphQL
 open FSharp.Data.GraphQL.Types
 open FSharp.Data.GraphQL.Relay
+open Common.Users
 
 module Resolvers =
   let Id = FSharp.Data.GraphQL.Types.SchemaDefinitions.ID<Guid>
 
-  type Person = {
-    id: Guid
-    name: string
-  }
+  let UserDef =
+    Define.Object<UserInfo>(
+      name = "User",
+      fields =
+        [ Define.AutoField("name", String)
+        ]
+    )
 
-  
+  let Query: UserInfo ObjectDef =
+    Define.Object(
+      "Query",
+      [ Define.Field(
+          "user",
+          UserDef,
+          "Looks up a user",
+          [
+          ],
+          fun ctx info ->
+            info
+        )
+      ]
+    )
+
+  let Mutation: UserInfo ObjectDef =
+    Define.Object(
+      "Mutation",
+      [ Define.Field(
+          "newUser",
+          Id,
+          "Creates a new user",
+          [ Define.Input("name", String)
+          ],
+          fun ctx info ->
+            System.Guid.NewGuid()
+        )
+      ]
+    )
+
+  let schema =
+    Schema(
+      query = Query,
+      mutation = Mutation
+    )
+  let exec = Executor schema
+
